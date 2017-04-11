b72b86fc18fce60059a628e8ec106a82a24d72ef
Alexander K <kymahek@mail.ru>
2017-04-11 10:46:53 +0300

    Multithreading level 7 tasks 2701-2707

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2707/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task27.task2707;
+
+import static java.lang.Thread.sleep;
+
+/*
+Определяем порядок захвата монитора
+*/
+public class Solution {
+    public void someMethodWithSynchronizedBlocks(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            synchronized (obj2) {
+                System.out.println(obj1 + " " + obj2);
+            }
+        }
+    }
+
+    public static boolean isNormalLockOrder(final Solution solution, final Object o1, final Object o2) throws Exception {
+        //do something here
+        solution.someMethodWithSynchronizedBlocks(o1,o2);
+        Thread thread = new Thread(() -> {
+            synchronized (o1)
+            {
+                synchronized (o2)
+                {
+                    try {
+                        sleep(100);
+                    }catch (InterruptedException e) {}
+                }
+            }
+        });
+        thread.start();
+        if (Thread.State.TIMED_WAITING.equals(thread.getState()))
+        {
+            return true;
+        } else return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Solution solution = new Solution();
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+
+        System.out.println(isNormalLockOrder(solution, o1, o2));
+    }
+}
