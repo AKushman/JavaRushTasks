41fc80c38e078ac772aac04967af549c8de78f3e
Alexander K <kymahek@mail.ru>
2017-04-18 10:03:24 +0300

    Multithreading lvl 8 BIG(HTML Editor) Finished

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,173 @@
+package com.javarush.task.task32.task3209;
+
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
+
+import javax.swing.*;
+import javax.swing.text.Document;
+import javax.swing.undo.UndoManager;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private JTabbedPane tabbedPane = new JTabbedPane(); //это будет панель с двумя вкладками.
+    private JTextPane htmlTextPane = new JTextPane();   //это будет компонент для визуального редактирования html. Он будет размещен на первой вкладке.
+    private JEditorPane plainTextPane = new JEditorPane(); //это будет компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            ExceptionHandler.log(e);
+        } catch (InstantiationException e) {
+            ExceptionHandler.log(e);
+        } catch (IllegalAccessException e) {
+            ExceptionHandler.log(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    private Controller controller;
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        String command = actionEvent.getActionCommand();
+        switch (command) {
+            case "Новый" :
+                controller.createNewDocument();
+                break;
+            case "Открыть" :
+                controller.openDocument();
+                break;
+            case "Сохранить" :
+                controller.saveDocument();
+                break;
+            case "Сохранить как..." :
+                controller.saveDocumentAs();
+                break;
+            case "Выход" :
+                controller.exit();
+                break;
+            case "О программе" :
+                showAbout();
+                break;
+        }
+    }
+
+    public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
+    public void initMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+        this.getContentPane().add(jMenuBar, BorderLayout.NORTH);
+    }
+
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", jScrollPane);
+        jScrollPane = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", jScrollPane);
+        tabbedPane.setPreferredSize(new Dimension());
+        TabbedPaneChangeListener listener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(listener);
+        this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void selectedTabChanged() {
+        if (tabbedPane.getSelectedIndex() == 0) {
+            String text = plainTextPane.getText();
+            controller.setPlainText(text);
+        }
+        else {
+            String text = controller.getPlainText();
+            plainTextPane.setText(text);
+        }
+        resetUndo();
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        }
+        catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        }
+        catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
+    }
+
+    public boolean isHtmlTabSelected() {
+        return tabbedPane.getSelectedIndex() == 0;
+    }
+
+    public void selectHtmlTab() {
+        tabbedPane.setSelectedIndex(0);
+        resetUndo();
+    }
+
+    public void update() {
+        Document document = controller.getDocument();
+        htmlTextPane.setDocument(document);
+    }
+
+    public void showAbout() {
+        JOptionPane.showMessageDialog(null, "My HTML Page editor", "About", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
