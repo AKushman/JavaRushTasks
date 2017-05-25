6bb183e3371d17b60fd08605e3c8552ec8dba437
Alexander Kushman <kymahek@mail.ru>
2017-05-25 17:28:11 +0300

    Java Collections 6 lvl before MVC refactoring

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/GenericStatic.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task35.task3501;
+
+
+public class GenericStatic<T> {
+
+    public static <T> T someStaticMethod(T genericObject) {
+        System.out.println(genericObject);
+        return genericObject;
+    }
+}
